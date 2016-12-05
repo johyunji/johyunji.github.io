dec3460b65b00fe6eb94e24176bd3059a6184461
johyunji <cookie671@naver.com>
2016-12-06 03:04:33 +0900

    12/06
    1. 슬라이드 이동 시 불필요한 oldActive 클래스 추가 부분 삭제
    2. 중복되는 animate를 each 함수로 변경
    3. 백그라운드 애니메이션 fade out 시 불필요한 부분 삭제
    4. 차트 리스트 + animate 추가

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-	
-	/* section 리사이즈 */
-	$(window).resize(function() {
-		$('section').height($(window).height() - $('section').offset().top); //윈도우 크기가 변경되면 섹션의 높이를 윈도우 높이와 동일하게
-	});
-	$(window).resize();
-	
 	/* 슬라이더 */
 	
 	var autoswitch = false; //자동 슬라이드
 	var autoswitch_speed = 8000; //슬라이드 속도
 	var isAnimating = false; //다중클릭 방지
+	
+	var $first = $('.card:first'); //첫번째 카드
+	var $last = $('.card:last'); //마지막 카드
 	
 	//첫번째 카드 활성화
 	$('.card').first().addClass('active');
@@ -20,18 +16,18 @@ $(document).ready(function() {
 	
 	//첫번째 카드 보이기
 	$('.active').animate({opacity:1}, function() {
-		activeAnimating(); //active 카드 애니메이션
+		activeOn(); //active 카드 애니메이션
 	});
 	
 	//이전 버튼 클릭 시 이전 카드로 슬라이드
-	$('.prevWrap').on('click', function() {
+	$('.prevWrap').click(function() {
 		if (isAnimating === false) { //애니메이팅이 종료됐을 때만 동작
 			prevSlide();
 		}
 	});
 	
 	//다음 버튼 클릭 시 다음 카드로 슬라이드
-	$('.nextWrap').on('click', function() {
+	$('.nextWrap').click(function() {
 		if (isAnimating === false) { //애니메이팅이 종료됐을 때만 동작
 			nextSlide();
 		}
@@ -68,35 +64,36 @@ $(document).ready(function() {
 
 	//다음으로 슬라이드
 	function nextSlide() {
-		//슬라이드 이동 시 현재 active 상태인 카드를 비활성화 하고, oldActive 클래스 부여
-		$('.active').removeClass('active').addClass('oldActive');
+		var $next, $active = $('.active');
 		
-		//카드가 마지막 장인지 확인
-		if ($('.oldActive').is(':last-child')) {
-			$('.card').first().addClass('active'); //현재 카드가 마지막 장이면, 첫번째 카드를 active
+		//마지막 카드인지 확인
+		if ($('.active').is($last)) {
+			$next = $first;
 		} else {
-			$('.oldActive').next().addClass('active'); //현재 카드가 마지막 장이 아니면, 다음 카드를 active
+			$next = $active.next();
 		}
 		
-		$('.oldActive').removeClass('oldActive'); //oldActive 된 카드의 클래스 삭제
-		
-		cardAnimating(); //현재 카드 퇴장 애니메이팅 시작
+		$active.removeClass('active');
+		$next.addClass('active');
+
+		activeOff(); //현재 카드 퇴장
 	}
 	
 	//이전으로 슬라이드
 	function prevSlide() {
-		$('.active').removeClass('active').addClass('oldActive');
+		var $prev, $active = $('.active');
 		
-		//카드가 첫번째 장인지 확인
-		if ($('.oldActive').is(':first-child')) {
-			$('.card').last().addClass('active'); //현재 카드가 첫번째 장이면, 마지막 카드를 active
+		//첫번째 카드인지 확인
+		if ($('.active').is($first)) {
+			$prev = $last;
 		} else {
-			$('.oldActive').prev().addClass('active'); //현재 카드가 첫번째 장이 아니면, 이전 카드를 active
+			$prev = $active.prev();
 		}
 		
-		$('.oldActive').removeClass('oldActive');
-		
-		cardAnimating(); //현재 카드 퇴장 애니메이팅 시작
+		$active.removeClass('active');
+		$prev.addClass('active');
+
+		activeOff(); //현재 카드 퇴장
 	}
 
 	/* 애니메이팅 */
@@ -107,90 +104,92 @@ $(document).ready(function() {
 	$('.next').animate({opacity:1,right:'-10px'},{duration:600,easing:'easeOutCubic'});
 	$('.nextWrap').animate({right:0},{duration:600,easing:'easeOutCubic'});
 	
-	//active 카드 등장
-	function activeAnimating() {
-		//active 카드의 오디오 재생
+	//active 등장
+	function activeOn() {
+		//애니메이팅 시작
+		isAnimating = true;
+		
+		//오디오 재생
 		var audio = $(".active audio")[0];
 		audio.play();
 		
 		//배경이미지 변경
-		var $img = $('.active .artistimg').children('img').attr('src');
+		var $active = $('.active');
+		var $img = $active.find('.artistimg').children('img').attr('src');
 		$(".bg").fadeOut(function() {
 			$(this).css({'background-image':'url('+$img+')'}).fadeIn(); //현재 카드의 아티스트이미지가 bg의 백그라운드 이미지로 페이드인
 		});
 		
-		//active 카드 애니메이팅
-		$('.active .box1').delay(150).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic',function(){
-			//애니메이팅 시작
-			isAnimating = true;
+		//active 애니메이팅
+		$active.find('.box1, .number, .artistimg, .box2, .artist, .title, .desc, .box3, .albumart, .times, .listened').each(function(i) {
+			$(this).delay(150 + i*150).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
+		});
+		$active.find('.playbtn').delay(1900).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic',complete:function() {
+			isAnimating = false; //애니메이팅 종료
 		}});
-		$('.active .number').delay(300).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .artistimg').delay(450).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .box2').delay(600).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .artist').delay(750).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .title').delay(900).animate({opacity:1},{duration:600,easing:'easeOutCubic'});
-		$('.active .desc').delay(1050).animate({opacity:1},{duration:600,easing:'easeOutCubic'});
-		$('.active .box3').delay(1300).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .albumart').delay(1450).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .times').delay(1600).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		$('.active .listened').delay(1750).animate({opacity:1},{duration:600,easing:'easeOutCubic'});
-		$('.active .playbtn').delay(1900).animate({opacity:1,width:'100%'},{duration:600,easing:'easeOutCubic'});
-		
-		//애니메이팅 종료
-		isAnimating = false;
 	};
 	
-	//기존 카드 퇴장 후 active 카드 등장
-	function cardAnimating() {
+	//active 퇴장
+	function activeOff() {
 		//애니메이팅 시작
 		isAnimating = true;
 		
-		//모든 카드의 오디오 볼륨이 페이드아웃 되며 정지
+		//모든 오디오의 볼륨이 페이드아웃 되며 정지
 		var audio = $(".card audio");
 		audio.animate({volume:0},1500,function() {
-			for(i=0; i<audio.length; i++) audio[i].pause(); //오디오 일시정지 후,
-			for(i=0; i<audio.length; i++) audio[i].currentTime = 0; //처음으로 되감기 함으로써 정지와 같은 효과
-			for(i=0; i<audio.length; i++) audio[i].volume = 1; //0이 되었던 볼륨을 1로 되돌림
+			for(i=0; i<audio.length; i++){
+				audio[i].pause(); //오디오 일시정지 후,
+				audio[i].currentTime = 0; //처음으로 되감기 함으로써 정지와 같은 효과
+				audio[i].volume = 1; //0이 되었던 볼륨을 1로 되돌림
+			}
 		});
 		
 		//배경이미지 변경
-		var $img = $('.active .artistimg').children('img').attr('src');
-		$(".bg").stop().delay(1900).fadeOut(function() {
-			$(this).css({'background-image':'none'}).fadeIn(); //bg의 백그라운드 이미지 지우기
-		});
+		$(".bg").stop().delay(1900).fadeOut(); //bg의 백그라운드 이미지 지우기
 		
-		//기존 카드 애니메이팅 (active될 때의 역순으로)
+		//퇴장 애니메이팅 (active될 때의 역순으로)
 		$('.box1').delay(1900).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic',complete:function() { //가장 마지막에 움직이는 box1의 애니메이팅이 끝나면
+			$('.playbtn').removeClass('play'); //플레이버튼을 클릭했을 경우, 다음 active되는 카드에서 play 클래스가 추가돼있는 것을 방지하기 위해 클래스 삭제로 초기화 해줌
 			$('.card').stop().animate({opacity:0},{complete:function() { //카드 숨기고
-				$('.active').stop().animate({opacity:1},{complete:function() { //active된 카드만 보이기
-					activeAnimating(); //active 카드 등장
-					isAnimating = false; //애니메이팅 종료
+				isAnimating = false; //애니메이팅 종료
+				$('.active').stop().animate({opacity:1},{complete:function() { //active만 보이기
+					activeOn(); //active 등장
 				}});
 			}});
 		}});
-		$('.number').delay(1750).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.artistimg').delay(1600).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.box2').delay(1450).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.artist').delay(1300).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.title').delay(1050).animate({opacity:0},{duration:600,easing:'easeOutCubic'});
-		$('.desc').delay(900).animate({opacity:0},{duration:600,easing:'easeOutCubic'});
-		$('.box3').delay(750).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.albumart').delay(600).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.times').delay(450).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
-		$('.listened').delay(300).animate({opacity:0},{duration:600,easing:'easeOutCubic'});
-		$('.playbtn').delay(150).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic',complete:function() {
-			$('.playbtn').removeClass('play'); //플레이버튼을 클릭했을 경우, 다음 active되는 카드에서 play 클래스가 추가돼있는 것을 방지하기 위해 클래스 삭제로 초기화 해줌
-		}});
+		
+		var $active = $('.active');
+		var $next = $active.next();
+		var $prev = $active.prev();
+		$first.add($last).add($next).add($prev).find('.title, .desc').delay(900).animate({opacity:0},{duration:600,easing:'easeOutCubic'});
+		$first.find('.number, .artistimg, .box2, .artist, .box3, .albumart, .times, .listened, .playbtn').each(function(i) {
+			$(this).delay(1750 - i*150).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
+		});
+		$last.find('.number, .artistimg, .box2, .artist, .box3, .albumart, .times, .listened, .playbtn').each(function(i) {
+			$(this).delay(1750 - i*150).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
+		});
+		$next.find('.number, .artistimg, .box2, .artist, .box3, .albumart, .times, .listened, .playbtn').each(function(i) {
+			$(this).delay(1750 - i*150).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
+		});
+		$prev.find('.number, .artistimg, .box2, .artist, .box3, .albumart, .times, .listened, .playbtn').each(function(i) {
+			$(this).delay(1750 - i*150).animate({opacity:0,width:0},{duration:600,easing:'easeOutCubic'});
+		});
 	}
 	
 	/* 버튼 클릭 시 오디오 재생/일시정지 토글 */
-	$('.playbtn').click(function() {
-		$('.playbtn').toggleClass('play'); //클릭 시 플래이버튼에 play 클래스가 토글
-		if ($('.playbtn').hasClass('play')) {
-			$('.active audio')[0].pause();        
+	var playbtn = $('.playbtn');
+	playbtn.click(function() {
+		playbtn.toggleClass('play'); //클릭 시 플래이버튼에 play 클래스가 토글
+		if (playbtn.hasClass('play')) {
+			$('.active audio')[0].pause();
 		} else {
 			$('.active audio')[0].play();
 		}
+	});
+	
+	/* 리스트 애니메이트 */
+    $('.chartList tr').each(function(i) {
+		$(this).delay(300 + i*150).animate({opacity:1,left:0},{duration:600,easing:'easeOutCubic'});
 	});
 	
 	/* 마우스 감지 parallax 플러그인 */
@@ -200,5 +199,9 @@ $(document).ready(function() {
 		$('.number, .box2, .audio, .albumart, .count').parallax(-20,e);
 		$('.artist, .title, .desc').parallax(-10,e);
 	});
-    
 });
+/* section 리사이즈 */
+$(window).resize(function() {
+	$('section').height($(window).height() - $('section').offset().top); //윈도우 크기가 변경되면 섹션의 높이를 윈도우 높이와 동일하게
+});
+$(window).resize();
